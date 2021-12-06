4022013b99c3062fb8f153d6b48fd58ebd7fe78a
Juan Vargas <juandeveloper19@gmail.com>
2021-12-06 14:18:33 -0500

    Component to search cryptocurrencies

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { TableCrypto } from './components/TableCrypto';
 import { getData } from './helper/getData';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SearchCrypto } from './components/SearchCrypto';
 
 const App = () => {
 	const [crypto, setCrypto] = useState([]);
@@ -16,6 +17,7 @@ const App = () => {
 	return (
 		<div className='container'>
 			<div className='row'>
+				<SearchCrypto />
 				<TableCrypto crypto={crypto} />
 			</div>
 		</div>
---
+++ src/components/SearchCrypto.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SearchCrypto = () => {
+	return <input type='text' placeholder='Search a Crypto' className='form-control bg-dark text-light border-0 mt-4 text-center' />;
+};
