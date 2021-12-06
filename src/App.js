f26ecb33edeb5031d7cfd7087e1cbcbe25a8e237
Juan Vargas <juandeveloper19@gmail.com>
2021-12-06 13:44:32 -0500

    Component To display the data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import { TableCrypto } from './components/TableCrypto';
 import { getData } from './helper/getData';
 
 const App = () => {
+	const [crypto, setCrypto] = useState([]);
+
 	useEffect(() => {
 		const response = getData();
 
-		response.then(({ data }) => console.log(data));
+		response.then(({ data }) => setCrypto(data));
 	}, []);
 
 	return (
 		<div>
 			<h1>Cripto Prices</h1>
+			<TableCrypto crypto={crypto} />
 		</div>
 	);
 };
---
+++ src/components/TableCrypto.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const TableCrypto = ({ crypto }) => {
+	console.log(crypto);
+	return <h3>Table Crypto</h3>;
+};
